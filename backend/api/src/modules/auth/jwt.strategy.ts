872c1505c21058e0e62855b2944c8d620bb8616e
jcrosario <jeancarlo.rosario@gmail.com>
2026-01-08 10:24:11 -0300

    Dash com lista e add de times

--- backend/api/src/modules/auth/jwt.strategy.ts
+++ backend/api/src/modules/auth/jwt.strategy.ts
@@ -12,6 +12,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return payload;
+    return {
+      userId: payload.sub,
+      email: payload.email,
+      tenant: payload.tenant,
+    };
   }
 }
